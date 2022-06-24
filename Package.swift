812518f69428d28e2484c0a82aca2bffb06519f7
Ron Daryl Magno <ron.zohan@gmail.com>
2022-06-24 11:06:22 +0800

    Update file checksums

--- Package.swift
+++ Package.swift
@@ -41,28 +41,28 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/6.0.0/GoogleMaps.xcframework.zip",
-            checksum: "cd775df376017bf507d3f0bdfc29a60ebbe35f58a44333a54a7786c07c546036"
+            url: "https://github.com/ronzohan/GoogleMaps-SP/releases/download/6.2.1-beta/GoogleMaps.xcframework.zip",
+            checksum: "28cb5c185a3e185af798975736d875f29699dae85c3cf876f5229fecce006438"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/6.0.0/GoogleMapsBase.xcframework.zip",
-            checksum: "37bbaf7a16c9495a791166f2ea64835bd8506034deb2f0970ec2c70b98cb310f"
+            url: "https://github.com/ronzohan/GoogleMaps-SP/releases/download/6.2.1-beta/GoogleMapsBase.xcframework.zip",
+            checksum: "298c8a4ad4cc2cd106b751a3e9eb229316b290807f4d969f7efd4aa968cdb32d"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/6.0.0/GoogleMapsCore.xcframework.zip",
-            checksum: "3c628402426869708f2b160e51526b5088e8d7a398b454ec7cba2ba11729cdb2"
+            url: "https://github.com/ronzohan/GoogleMaps-SP/releases/download/6.2.1-beta/GoogleMapsCore.xcframework.zip",
+            checksum: "10b2a070d02d38355516f2dbb58b3efc263a34a8f097cef327ca929a39e8bb49"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/6.0.0/GoogleMapsM4B.xcframework.zip",
-            checksum: "3d7a6832b9f9a47b4ea2c82a982e5084411d8b1bf6371855edc1f434ddbe96c0"
+            url: "https://github.com/ronzohan/GoogleMaps-SP/releases/download/6.2.1-beta/GoogleMapsM4B.xcframework.zip",
+            checksum: "43927f9fcb677efd3252d8c145799d20550e9b1f9af62d5825e65828e396ab4c"
         ),
         .binaryTarget(
             name: "GooglePlaces",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/6.0.0/GooglePlaces.xcframework.zip",
-            checksum: "50ebe575f4612e0c52ad2d388dbe25c943b88c0bf9a9a016b340dcca711e6bc6"
+            url: "https://github.com/ronzohan/GoogleMaps-SP/releases/download/6.2.1-beta/GooglePlaces.xcframework.zip",
+            checksum: "82fbff094c48180ddd49677505c5761a131e63ec1d5a8b4bf6df0902dc8e86be"
         )
     ]
 )
